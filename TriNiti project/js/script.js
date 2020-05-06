6cba9f9fefc903595819014ca206fcb09b2e4a8c
uskrupnik <uskrupnik@gmail.com>
2020-05-06 14:09:40 +0300

    555 triniti

---
+++ TriNiti project/js/script.js
@@ -0,0 +1,97 @@
+"use strict";
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		adeptiveHeight: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 600,
+		easing: 'ease',
+		infinite: true,
+		waitForAnimate: false,
+		asNavFor: ".sliderbig",
+		focusOnSelect: true
+	});
+	$('.sliderbig').slick({
+		fade: true,
+		slidesToShow: 1,
+		asNavFor: ".slider",
+		infinite: true,
+
+	})
+	$('.sliderslick').slick({
+		arrows: true,
+		dots: false,
+		adeptiveHeight: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		infinite: true,
+		speed: 600,
+		easing: 'ease-in ',
+		autoplay: true,
+		autoplaySpeed: 2000,
+		touchThreshold: 100,
+		waitForAnimate: false,
+		responsive: [
+			{
+				breakpoint: 980,
+				settings: {
+					arrows: false,
+					centerPadding: '40px',
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 760,
+				settings: {
+					arrows: false,
+					centerPadding: '40px',
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 460,
+				settings: {
+					arrows: false,
+					centerPadding: '40px',
+					slidesToShow: 1,
+				}
+			}
+		]
+	})
+});
+
+
+document.body.style.background = '#ffe3cb';
+
+
+let color = document.querySelectorAll('.contaner__wrap');
+
+for (let key of color) {
+	key.style.background = '#ffe3cb';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
